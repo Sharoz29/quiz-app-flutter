ffbade4558359105abca0eb2e683eb37540d5762
Malik Sharoz Tariq <maliksharoz70@gmail.com>
2024-02-29 01:59:51 +0500

    Moving starting screen to own widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,21 @@
 import "package:flutter/material.dart";
-import "package:quiz_app/gap_box.dart";
-import "package:quiz_app/custom_text_box.dart";
+import "package:quiz_app/starting_screen.dart";
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                './assets/images/quiz-logo.png',
-                width: 200,
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168)
+                ],
               ),
-              const GapBox(
-                givenHeight: 100,
-              ),
-              const CustomText(
-                  text: "Learn Flutter the fun way!",
-                  color: Colors.white,
-                  fontSize: 25),
-              const GapBox(givenHeight: 50),
-              TextButton(
-                onPressed: () => {},
-                child: const CustomText(
-                    text: "Start Quiz", color: Colors.white, fontSize: 15),
-              )
-            ],
-          ),
-        ),
+            ),
+            child: const StartingScreen()),
       ),
     ),
   );
